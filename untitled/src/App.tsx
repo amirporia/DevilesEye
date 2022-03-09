78c04a09f57747ec17bc31c3208fe762546415b5
amirporia99.1378@gmail.com <amirporia99.1378@gmail.com>
2022-03-09 08:59:28 +0330

    Commit

---
+++ untitled/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ScrollUpButton from 'devileye';
+
+function App() {
+    return (
+        <div>
+            <ScrollUpButton>
+
+            </ScrollUpButton>
+        </div>
+    );
+}
+
+export default App;
